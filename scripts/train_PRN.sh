69e5160481eb7aec61a32e84cdd5d4eae6116c14
kim-sunghoon <sunghoon1kim@postech.ac.kr>
2019-11-16 18:43:43 +0900

    train_PRN.sh - raindrop test를 위해 코드 조금 수정함

--- scripts/train_PRN.sh
+++ scripts/train_PRN.sh
@@ -1,10 +1,13 @@
 #! bash
 
 # Rain100H
-python train_PRN.py --save_path logs/Rain100H/PRN --data_path datasets/train/RainTrainH
+# python train_PRN.py --save_path logs/Rain100H/PRN --data_path datasets/train/RainTrainH --gpu_id 0 --preprocess False
 
 # Rain100L
-# python train_PRN.py --save_path logs/Rain100L/PRN --data_path datasets/train/RainTrainL
+# python train_PRN.py --save_path logs/Rain100L/PRN --data_path datasets/train/RainTrainL --gpu_id 1 --preprocess False
 #
 # # Rain12600
-# python train_PRN.py --save_path logs/Rain1400/PRN --data_path datasets/train/Rain12600
+# python train_PRN.py --save_path logs/Rain1400/PRN --data_path datasets/train/Rain12600 --gpu_id 2 --preprocess False
+
+## rain drop
+python train_PRN.py --save_path logs/Raindrop/PRN --data_path datasets/train/raindrop --gpu_id 3 --preprocess True
