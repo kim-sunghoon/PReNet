6372114946b02e387e9e1082e87b56dcb901516d
kim-sunghoon <sunghoon1kim@postech.ac.kr>
2019-11-29 16:13:32 +0900

    lossv2로 코드 바꿈 batch size 18->36 올림

--- train_raindrop.sh
+++ train_raindrop.sh
@@ -1,23 +1,27 @@
 #! bash
 
-batch=18
+batch=36
 data_path=datasets/train/raindrop2
 ## APRN_r
-echo "train"
+echo "train ARPN6_r_2_l1"
 # echo "python train_APRN_r.py --save_path logs/raindrop/APRN6_r --data_path datasets/train/raindrop2 --batch_size 18 --gpu_id 1"
 # python train_APRN_r.py --save_path logs/raindrop/APRN6_r --data_path datasets/train/raindrop2 --batch_size 18 --gpu_id 1
 # echo ", which is already done! where loss is ssim"
 # echo "Too large attention module... "
-attention2_save_path=logs/raindrop/APRN6_r_2
+# attention2_save_path=logs/raindrop/APRN6_r_2
 # echo "python train_APRN_r.py --save_path $attention2_save_path --data_path $data_path --batch_size $batch --gpu_id 1"
 # python train_APRN_r.py --save_path $attention2_save_path --data_path $data_path --batch_size $batch --gpu_id 1
 
-attention2_w_loss_save_path=logs/raindrop/APRN6_r_2_l1
+# attention2_w_loss_save_path=logs/raindrop/APRN6_r_2_l1
 echo "Now, start adapt different loss function"
-echo "python train_APRN_r.py --save_path $attention2_w_loss_save_path --data_path $data_path --batch_size $batch --gpu_id 1"
-python train_APRN_r.py --save_path $attention2_w_loss_save_path --data_path $data_path --batch_size $batch --gpu_id 1
+# echo "python train_APRN_r.py --save_path $attention2_w_loss_save_path --data_path $data_path --batch_size $batch --gpu_id 1"
+# python train_APRN_r.py --save_path $attention2_w_loss_save_path --data_path $data_path --batch_size $batch --gpu_id 1
 
+attention2_w_loss_save_path2=logs/raindrop/APRN6_r_2_l2
+echo "python train_APRN_r.py --save_path $attention2_w_loss_save_path2 --data_path $data_path --batch_size $batch --gpu_id 1"
+python train_APRN_r.py --save_path $attention2_w_loss_save_path2 --data_path $data_path --batch_size $batch --gpu_id 1
 
+#####################################
 ## PRN
 # python train_PRN.py --save_path logs/raindrop/PRN6 --data_path datasets/train/raindrop1 --batch_size 36 --gpu_id 0
 # python train_PRN.py --save_path logs/raindrop/PRN6 --data_path datasets/train/raindrop --batch_size 36 --gpu_id 0 --preprocess True
